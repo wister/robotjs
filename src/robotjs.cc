8a709091533e2a2ead5b786724577c35c1bd6172
Wister <wisterfungfung@gmail.com>
2019-11-08 04:50:33 -0500

    Update robotjs.cc

--- src/robotjs.cc
+++ src/robotjs.cc
@@ -433,10 +433,9 @@ int GetFlagsFromValue(v8::Local<v8::Value> value, MMKeyFlags* flags)
 	if (value->IsArray())
 	{
 		v8::Local<v8::Array> a = v8::Local<v8::Array>::Cast(value);
-		//v8::Local<v8::Context> context;
 		for (uint32_t i = 0; i < a->Length(); i++)
 		{
-			v8::Local<v8::Value> v(a->Nan::Get(i));
+			v8::Local<v8::Value> v(Nan::Get(a, i));
 			if (!v->IsString()) return -2;
 
 			MMKeyFlags f = MOD_NONE;
