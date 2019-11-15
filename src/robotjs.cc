8233f1752948767b146ea86dcd815d02501e0641
Wister <wisterfungfung@gmail.com>
2019-11-15 00:38:05 -0500

    Update robotjs.cc

--- src/robotjs.cc
+++ src/robotjs.cc
@@ -872,4 +872,4 @@ NAN_MODULE_INIT(InitAll)
 		Nan::GetFunction(Nan::New<FunctionTemplate>(setXDisplayName)).ToLocalChecked());
 }
 
-NAN_MODULE_WORKER_ENABLED(NODE_GYP_MODULE_NAME, InitAll)
+NAN_MODULE_WORKER_ENABLED(robotjs, InitAll)
